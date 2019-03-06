6bbadbcbafe2142212b3355d3282b67044c96bc6
srikanthvoru <srikanthvoru42>
2019-03-06 15:42:13 +0530

    this is weather app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDoqQGth97IozXaxCDFVV_pDpegNvVIFe4",
+    authDomain: "weatherapi2-b2367.firebaseapp.com",
+    databaseURL: "https://weatherapi2-b2367.firebaseio.com",
+    projectId: "weatherapi2-b2367",
+    storageBucket: "weatherapi2-b2367.appspot.com",
+    messagingSenderId: "822203644332"
+  }
+};
